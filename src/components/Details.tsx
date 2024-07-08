6927d9a25c35d17516b11e6ebbca12e429452624
runraga <james@helmfarm.com>
2024-07-08 09:37:48 +0100

    update cv link

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -40,7 +40,7 @@ const contactLinks: Link[] = [
   },
   {
     icon: MdImportContacts,
-    url: "https://portfolio.helmfarm.com/cv/cv.pdf",
+    url: "./cv/cv.pdf",
     text: "Download CV",
   },
 ];
