2390fec7b751dd0dd6e8ee5fbc21d2209784ec94
runraga <james@helmfarm.com>
2024-10-03 15:42:28 +0100

    Update about me

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -13,25 +13,24 @@ const AboutMe = () => {
       <Stack>
         <Text>
           Ever since I was a kid I've loved solving puzzles. Then it was
-          matchsticks or maths problems set by my dad. As a structural biologist
-          and lab manager the puzzle has been in handling, processing and
-          interpreting complex data to advance scientific knowledge and aid
+          matchsticks, logic or maths problems set by my dad. As a structural
+          biologist and lab manager the puzzle has been in handling, processing
+          and interpreting complex data to advance scientific knowledge and aid
           strategic decision making.
         </Text>
         <Text>
-          Learning to code has greatly aided in leveraging this data to its maximum
-          potential and given a huge boost to productivity and sense of
-          achievement. Consequently I decided to put coding/data engineering at
-          the centre of my future career plans. I completed a three month data
-          engineering bootcamp with Northcoders in February 2024 which has
-          provided an invaluable foundation to my career 'pivot'.
+          Learning to code has has been a hugely rewarding experience and has
+          allowed me to leverage complex data to its maximum potential. It has
+          given a huge boost to productivity and sense of achievement.
+          Consequently I decided to put coding/data engineering at the centre of
+          my future career plans. I completed a three month data engineering
+          bootcamp with Northcoders in February 2024 which has provided an
+          invaluable foundation to my career 'pivot'.
         </Text>
         <Text>
           I am now looking to combine my structural biology and lab management
-          skills with software development to find innovative
-          solutions in data science, focussing on big data to help scientists
-          draw sound conclusions advancing scientific research and operational
-          efficiency.
+          skills with software development and data engineering to find
+          innovative solutions to real-world problems of the highest complexity.
         </Text>
       </Stack>
     </Card>
